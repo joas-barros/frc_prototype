ffc8689ddb62c1571f1c09d62f26415aad0c20c9
joas-barros <joas.barros14@hotmail.com>
2025-04-19 11:33:13 -0300

    feat: supabase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
 import 'package:florida_rental_car/app/app_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: ".env");
+
+  String supabaseUrl = dotenv.env['SUPABASE_URL'] ?? '';
+  String supabaseKey = dotenv.env['SUPABASE_KEY'] ?? '';
+  await Supabase.initialize(
+    url: supabaseUrl,
+    anonKey: supabaseKey,
+  );
+  
   runApp(const MyApp());
 }
 
