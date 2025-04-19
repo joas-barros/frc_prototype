c61f3eafe746b26713f2055977699b0cde6e9fac
joas-barros <joas.barros14@hotmail.com>
2025-04-19 19:40:23 -0300

    feat: add core configuration files

---
+++ lib/app/ui/core/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color loginBackground = Color(0xFF272739);
+  static const Color buttonBackground = Color(0xFFC90000);
+  static const Color buttonText = Color(0xFFFFFFFF);
+  static const Color textFieldBackground = Color(0xFF3A3B4D);
+}
