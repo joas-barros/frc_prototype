bbf9a8ae5b1529b3443bc00439bed477b8b68dd7
joas-barros <joas.barros14@hotmail.com>
2025-04-20 14:41:29 -0300

    style: ForgotPasswordMessage page

---
+++ lib/app/ui/pages/forgot_password/forgot_password_message.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+class ForgotPasswordMessage extends StatelessWidget {
+  const ForgotPasswordMessage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Recuperar Senha',
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: 8),
+            const Text(
+              'Link de redefinição enviado',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 16,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              spacing: 16,
+              children: [
+                const Text(
+                  'Enviamos um email para você redefinir sua senha.',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const Text(
+                  'Para criar uma nova senha, clique no link do e-mail e insira uma nova senha.',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                const Text(
+                  'Caso não tenha recebido o e-mail, verifique o seu lixo eletrônico ou clique abaixo para reenviar o email',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                textStyle: const TextStyle(
+                  fontSize: 12,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              child: Text("Reenviar e-mail"),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: ElevatedButton(
+        onPressed: () {},
+        child: Text("Entrar"),
+      ),
+    );
+  }
+}
